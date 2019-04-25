062134d94d4c0cd00270d2f0c8b472bd40e5795b
Daniel Armstrong <danielarmstrong124@gmail.com>
2019-04-25 22:07:47 +0000

    Updated sendMail.js parameters

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,13 +1,15 @@
 function sendMail(contactForm){
+
 var template_params = {
-    "from_name": contactForm.name.value,
-    "from_email": contactForm.email.value,
-    "from_mobile": contactForm.mobile.value,
-    "deadline": contactForm.deadline.value,
-    "project_request": contactForm.projectsummary.value
+   "from_name": contactForm.name.value,
+   "from_email": contactForm.email.value,
+   "project_request": contactForm.projectrequest.value,
+   "deadline": contactForm.deadline.value,
+   "from_mobile": contactForm.mobile.value
 }
 
 var service_id = "default_service";
 var template_id = "danielarmstrong";
 emailjs.send(service_id, template_id, template_params);
+
 }
