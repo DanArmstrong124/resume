8d660f2e9e02ef9d31fda483d25036305d97e185
Daniel Armstrong <danielarmstrong124@gmail.com>
2019-06-06 15:59:26 +0000

    Added templateSite.js/css/html

---
+++ templatesite.js
@@ -0,0 +1,15 @@
+$("#red").on('click', function() {
+    $("#bodymain").css("background-color", "red");
+});
+
+$("#blue").on('click', function() {
+    $("#bodymain").css("background-color", "blue");
+});
+
+$("#yellow").on('click', function() {
+    $("#bodymain").css("background-color", "yellow");
+});
+
+$("#green").on('click', function() {
+    $("#bodymain").css("background-color", "green");
+});
