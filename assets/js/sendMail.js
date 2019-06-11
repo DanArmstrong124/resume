d99c718eb4fd5edd0bd97d3817b013fd0474a453
Daniel Armstrong <danielarmstrong124@gmail.com>
2019-06-11 16:34:39 +0000

    Added FEEDBACK EmailJS

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -22,3 +22,29 @@ function sendMail(contactForm) {
         );
     return false; // To block from loading a new page
 }
+
+function sendMailFeedback(contactForm) {
+
+    emailjs.send("default_service", "template_KJuK8WhN", {
+            "from_name": contactForm.name.value,,
+            "from_email": contactForm.email.value,,
+            "feedbackprojectname": contactForm.feedbackprojectname.value,
+            "from_mobile": contactForm.mobile.value,
+            "whatdidyoulike": contactForm.whatdidyoulike.value,
+            "whatdidntyoulike": contactForm.whatdidntyoulike.value,
+            "whatshouldiadd": contactForm.whatshouldiadd.value,
+            "whatshouldiremove": contactForm.whatshouldiremove.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                $("#details-sent").removeClass("invisable").addClass("visable");
+                $("#up-to-5").removeClass("invisable").addClass("visable");
+                setTimeout(location.reload.bind(location), 600);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
